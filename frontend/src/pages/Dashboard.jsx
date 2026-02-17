36859618c9eadb3995dbb7231f3dfce25d7bc376
Deepak Joshi <joshideepu892@gmail.com>
2026-02-17 19:03:58 +0545

    dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,21 @@
-
-import { useEffect } from 'react';
-import UserList from '../components/UserList'
-import LeadsSummary from '../components/LeadsSummary';
+import { useEffect } from "react";
+import UserList from "../components/UserList";
+import LeadsSummary from "../components/LeadsSummary";
 
 const Dashboard = () => {
-    useEffect(() => {
-        window.scrollTo({
-            top: 0,
-            behavior: "instant"
-        })
-    }, [])
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
+  }, []);
 
-    return (
-        <div>
-            <LeadsSummary />
-            <UserList />
-        </div>
-
-    );
-}
+  return (
+    <div>
+      <LeadsSummary />
+      <UserList />
+    </div>
+  );
+};
 
 export default Dashboard;
